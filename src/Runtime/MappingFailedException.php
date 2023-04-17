99f17bc8e8cd37495a0e222a881c69feb33c63e8
Jan Tvrdík <git@jantvrdik.com>
2023-04-17 16:39:30 +0200

    MappingFailedException: format path as JSON pointer

--- src/Runtime/MappingFailedException.php
+++ src/Runtime/MappingFailedException.php
@@ -16,11 +16,11 @@ class MappingFailedException extends RuntimeException
 
     /**
      * @param list<string|int> $path
-     * @param T                  $data
+     * @param T                $data
      */
     public function __construct(mixed $data, array $path, string $expected = '', ?Throwable $previous = null)
     {
-        $message = sprintf('Failed to map data at path "%s": expected %s, got %s', implode('.', $path), $expected, json_encode($data));
+        $message = sprintf('Failed to map data at path /%s: expected %s, got %s', implode('/', $path), $expected, json_encode($data));
         parent::__construct($message, 0, $previous);
     }
 
