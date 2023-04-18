e760610b98bbd1eeb46e18470f599aea65e02883
Jan Tvrdík <git@jantvrdik.com>
2023-04-18 14:27:22 +0200

    add MapFloatTest

---
+++ tests/Compiler/Mapper/Scalar/Data/FloatMapper.php
@@ -0,0 +1,34 @@
+<?php declare (strict_types=1);
+
+namespace ShipMonkTests\InputMapper\Compiler\Mapper\Scalar\Data;
+
+use ShipMonk\InputMapper\Runtime\Mapper;
+use ShipMonk\InputMapper\Runtime\MapperProvider;
+use ShipMonk\InputMapper\Runtime\MappingFailedException;
+use function floatval;
+use function is_float;
+use function is_int;
+
+/**
+ * Generated mapper. Do not edit directly.
+ *
+ * @implements Mapper<float>
+ */
+class FloatMapper implements Mapper
+{
+    public function __construct(private readonly MapperProvider $provider)
+    {
+    }
+
+    /**
+     * @param  list<string|int> $path
+     */
+    public function map(mixed $data, array $path = []): float
+    {
+        if (!is_float($data) && !is_int($data)) {
+            throw MappingFailedException::incorrectType($data, $path, 'float');
+        }
+
+        return floatval($data);
+    }
+}
