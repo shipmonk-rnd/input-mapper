b90ecd60fe56beddb0bc40bdd4f72df01b12e36c
Jan Tvrdík <git@jantvrdik.com>
2023-04-18 14:36:25 +0200

    add MapNullableTest

---
+++ tests/Compiler/Mapper/Wrapper/Data/NullableIntMapper.php
@@ -0,0 +1,38 @@
+<?php declare (strict_types=1);
+
+namespace ShipMonkTests\InputMapper\Compiler\Mapper\Wrapper\Data;
+
+use ShipMonk\InputMapper\Runtime\Mapper;
+use ShipMonk\InputMapper\Runtime\MapperProvider;
+use ShipMonk\InputMapper\Runtime\MappingFailedException;
+use function is_int;
+
+/**
+ * Generated mapper. Do not edit directly.
+ *
+ * @implements Mapper<?int>
+ */
+class NullableIntMapper implements Mapper
+{
+    public function __construct(private readonly MapperProvider $provider)
+    {
+    }
+
+    /**
+     * @param  list<string|int> $path
+     */
+    public function map(mixed $data, array $path = []): ?int
+    {
+        if ($data === null) {
+            $mapped = null;
+        } else {
+            if (!is_int($data)) {
+                throw MappingFailedException::incorrectType($data, $path, 'int');
+            }
+
+            $mapped = $data;
+        }
+
+        return $mapped;
+    }
+}
---
+++ tests/Compiler/Mapper/Wrapper/MapNullableTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonkTests\InputMapper\Compiler\Mapper\Wrapper;
+
+use ShipMonk\InputMapper\Compiler\Mapper\Scalar\MapInt;
+use ShipMonk\InputMapper\Compiler\Mapper\Wrapper\MapNullable;
+use ShipMonk\InputMapper\Runtime\MappingFailedException;
+use ShipMonkTests\InputMapper\Compiler\Mapper\MapperCompilerTestCase;
+
+class MapNullableTest extends MapperCompilerTestCase
+{
+
+    public function testCompile(): void
+    {
+        $mapperCompiler = new MapNullable(new MapInt());
+        $mapper = $this->compileMapper('NullableInt', $mapperCompiler);
+
+        self::assertNull($mapper->map(null));
+        self::assertSame(1, $mapper->map(1));
+        self::assertSame(2, $mapper->map(2));
+
+        self::assertException(
+            MappingFailedException::class,
+            'Failed to map data at path /: Expected int, got "1"',
+            static fn() => $mapper->map('1'),
+        );
+
+        self::assertException(
+            MappingFailedException::class,
+            'Failed to map data at path /: Expected int, got array',
+            static fn() => $mapper->map([]),
+        );
+    }
+
+}
