42aa56e8bfc8371406cd44f839ed765b4abb0349
Jan Tvrdík <git@jantvrdik.com>
2023-04-18 17:31:35 +0200

    add AssertStringLengthTest

---
+++ tests/Compiler/Validator/String/Data/StringLengthValidatorWithMaxMapper.php
@@ -0,0 +1,35 @@
+<?php declare (strict_types=1);
+
+namespace ShipMonkTests\InputMapper\Compiler\Validator\String\Data;
+
+use ShipMonk\InputMapper\Runtime\Mapper;
+use ShipMonk\InputMapper\Runtime\MapperProvider;
+use ShipMonk\InputMapper\Runtime\MappingFailedException;
+use function is_string;
+use function strlen;
+
+/**
+ * Generated mapper. Do not edit directly.
+ *
+ * @implements Mapper<mixed>
+ */
+class StringLengthValidatorWithMaxMapper implements Mapper
+{
+    public function __construct(private readonly MapperProvider $provider)
+    {
+    }
+
+    /**
+     * @param  list<string|int> $path
+     */
+    public function map(mixed $data, array $path = []): mixed
+    {
+        if (is_string($data)) {
+            if (strlen($data) > 5) {
+                throw MappingFailedException::incorrectValue($data, $path, 'string with at most 5 characters');
+            }
+        }
+
+        return $data;
+    }
+}
