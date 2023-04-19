2613f736bb1c80cec7e5c8042f7af002b9e2c04a
Jan Tvrdík <git@jantvrdik.com>
2023-04-19 17:37:30 +0200

    add AssertRuntime

---
+++ src/Compiler/Validator/AssertRuntime.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\InputMapper\Compiler\Validator;
+
+use PhpParser\Node\Expr;
+use PhpParser\Node\Stmt;
+use ShipMonk\InputMapper\Compiler\Php\PhpCodeBuilder;
+use ShipMonk\InputMapper\Runtime\MappingFailedException;
+
+abstract class AssertRuntime implements ValidatorCompiler
+{
+
+    /**
+     * @param  list<string|int> $path
+     * @throws MappingFailedException
+     */
+    abstract public static function assertValue(mixed $value, array $path): void;
+
+    /**
+     * @return list<Stmt>
+     */
+    public function compile(
+        Expr $value,
+        Expr $path,
+        PhpCodeBuilder $builder,
+    ): array
+    {
+        return [
+            $builder->staticCall(
+                $builder->importClass(static::class),
+                'assertValue',
+                [$value, $path],
+            ),
+        ];
+    }
+
+    /**
+     * @param  array<string, mixed> $schema
+     * @return array<string, mixed>
+     */
+    public function toJsonSchema(array $schema): array
+    {
+        return $schema;
+    }
+
+}
---
+++ tests/Compiler/Validator/Data/MultipleOfSevenValidatorMapper.php
@@ -0,0 +1,30 @@
+<?php declare (strict_types=1);
+
+namespace ShipMonkTests\InputMapper\Compiler\Validator\Data;
+
+use ShipMonkTests\InputMapper\Compiler\Validator\Data\AssertMultipleOfSeven;
+use ShipMonk\InputMapper\Runtime\Mapper;
+use ShipMonk\InputMapper\Runtime\MapperProvider;
+use ShipMonk\InputMapper\Runtime\MappingFailedException;
+
+/**
+ * Generated mapper. Do not edit directly.
+ *
+ * @implements Mapper<mixed>
+ */
+class MultipleOfSevenValidatorMapper implements Mapper
+{
+    public function __construct(private readonly MapperProvider $provider)
+    {
+    }
+
+    /**
+     * @param  list<string|int> $path
+     * @throws MappingFailedException
+     */
+    public function map(mixed $data, array $path = []): mixed
+    {
+        AssertMultipleOfSeven::assertValue($data, $path);
+        return $data;
+    }
+}
