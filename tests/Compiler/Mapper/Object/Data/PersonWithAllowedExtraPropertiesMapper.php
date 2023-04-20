fbd0473f7a9514b7ddb6d93d7fa5689d063b982e
Jan Tvrdík <git@jantvrdik.com>
2023-04-20 19:13:06 +0200

    MapObjectTest: add test with allowed extra properties

---
+++ tests/Compiler/Mapper/Object/Data/PersonWithAllowedExtraPropertiesMapper.php
@@ -0,0 +1,90 @@
+<?php declare (strict_types=1);
+
+namespace ShipMonkTests\InputMapper\Compiler\Mapper\Object\Data;
+
+use ShipMonkTests\InputMapper\Compiler\Mapper\Object\Data\PersonInput;
+use ShipMonk\InputMapper\Runtime\Mapper;
+use ShipMonk\InputMapper\Runtime\MapperProvider;
+use ShipMonk\InputMapper\Runtime\MappingFailedException;
+use ShipMonk\InputMapper\Runtime\Optional;
+use function array_key_exists;
+use function is_array;
+use function is_int;
+use function is_string;
+
+/**
+ * Generated mapper. Do not edit directly.
+ *
+ * @implements Mapper<PersonInput>
+ */
+class PersonWithAllowedExtraPropertiesMapper implements Mapper
+{
+    public function __construct(private readonly MapperProvider $provider)
+    {
+    }
+
+    /**
+     * @param  list<string|int> $path
+     * @throws MappingFailedException
+     */
+    public function map(mixed $data, array $path = []): PersonInput
+    {
+        if (!is_array($data)) {
+            throw MappingFailedException::incorrectType($data, $path, 'array');
+        }
+
+        if (!array_key_exists('id', $data)) {
+            throw MappingFailedException::missingKey($path, 'id');
+        }
+
+        if (!array_key_exists('name', $data)) {
+            throw MappingFailedException::missingKey($path, 'name');
+        }
+
+        return new PersonInput(
+            $this->mapId($data['id'], [...$path, 'id']),
+            $this->mapName($data['name'], [...$path, 'name']),
+            array_key_exists('age', $data) ? $this->mapAge($data['age'], [...$path, 'age']) : Optional::none($path, 'age'),
+        );
+    }
+
+    /**
+     * @param  list<string|int> $path
+     * @return Optional<int>
+     * @throws MappingFailedException
+     */
+    private function mapAge(mixed $data, array $path = []): Optional
+    {
+        if (!is_int($data)) {
+            throw MappingFailedException::incorrectType($data, $path, 'int');
+        }
+
+        return Optional::of($data);
+    }
+
+    /**
+     * @param  list<string|int> $path
+     * @throws MappingFailedException
+     */
+    private function mapName(mixed $data, array $path = []): string
+    {
+        if (!is_string($data)) {
+            throw MappingFailedException::incorrectType($data, $path, 'string');
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param  list<string|int> $path
+     * @throws MappingFailedException
+     */
+    private function mapId(mixed $data, array $path = []): int
+    {
+        if (!is_int($data)) {
+            throw MappingFailedException::incorrectType($data, $path, 'int');
+        }
+
+        return $data;
+    }
+}
