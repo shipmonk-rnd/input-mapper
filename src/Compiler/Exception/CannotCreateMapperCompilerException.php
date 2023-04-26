189fcf44f96fc15a7afbd62b19848233585f07a5
Jan Tvrdík <git@jantvrdik.com>
2023-04-26 12:06:24 +0200

    fix errors

--- src/Compiler/Exception/CannotCreateMapperCompilerException.php
+++ src/Compiler/Exception/CannotCreateMapperCompilerException.php
@@ -11,7 +11,7 @@ class CannotCreateMapperCompilerException extends LogicException
 
     public static function fromType(TypeNode $type, ?string $reason = null, ?Throwable $previous = null): self
     {
-        $reason = $reason ? ", because {$reason}" : '';
+        $reason = $reason !== null ? ", because {$reason}" : '';
         return new self("Cannot create mapper for type {$type}{$reason}", 0, $previous);
     }
 
