6e721a3e05ce70b51b3f9f5f2e82b8c1dd3c2fd8
Jan Tvrdík <git@jantvrdik.com>
2023-05-09 13:07:05 +0200

    DefaultMapperCompilerFactory: read types from @var as well

---
+++ tests/Compiler/MapperFactory/Data/CarInputWithVarTags.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonkTests\InputMapper\Compiler\MapperFactory\Data;
+
+use ShipMonk\InputMapper\Compiler\Mapper\Array\MapList;
+use ShipMonk\InputMapper\Compiler\Mapper\Scalar\MapInt;
+use ShipMonk\InputMapper\Compiler\Mapper\Wrapper\ValidatedMapperCompiler;
+use ShipMonk\InputMapper\Compiler\Validator\Int\AssertPositiveInt;
+use ShipMonk\InputMapper\Compiler\Validator\String\AssertStringLength;
+use ShipMonk\InputMapper\Runtime\Optional;
+
+class CarInputWithVarTags
+{
+
+    public function __construct(
+        public readonly int $id,
+
+        #[AssertStringLength(exact: 7)]
+        public readonly string $name,
+
+        /** @var Optional<BrandInput> */
+        public readonly Optional $brand,
+
+        /** @var array<int> */
+        #[MapList(new ValidatedMapperCompiler(new MapInt(), [new AssertPositiveInt()]))]
+        public readonly array $numbers,
+    )
+    {
+    }
+
+}
