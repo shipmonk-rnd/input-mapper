fc5c259228fc8293669dcf4829238de1b856dac8
Jan Tvrdík <git@jantvrdik.com>
2023-05-19 15:44:47 +0200

    add MapDate

---
+++ src/Compiler/Mapper/Object/MapDate.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\InputMapper\Compiler\Mapper\Object;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PARAMETER)]
+class MapDate extends MapDateTimeImmutable
+{
+
+    public function __construct(
+        string $formatDescription = 'date string in Y-m-d format',
+        ?string $timezone = null,
+    )
+    {
+        parent::__construct('!Y-m-d', $formatDescription, $timezone);
+    }
+
+}
