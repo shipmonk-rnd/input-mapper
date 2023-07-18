145b8778d8b49daf7a4e16314ec8392a65dc7f08
Jan Tvrdík <git@jantvrdik.com>
2023-07-18 14:38:23 +0200

    ChainMapperCompiler: check mapper compatibility

---
+++ tests/Compiler/Mapper/Wrapper/Data/DoubleIntMapper.php
@@ -0,0 +1,33 @@
+<?php declare (strict_types=1);
+
+namespace ShipMonkTests\InputMapper\Compiler\Mapper\Wrapper\Data;
+
+use ShipMonk\InputMapper\Runtime\Exception\MappingFailedException;
+use ShipMonk\InputMapper\Runtime\Mapper;
+use ShipMonk\InputMapper\Runtime\MapperProvider;
+use function is_int;
+
+/**
+ * Generated mapper. Do not edit directly.
+ *
+ * @implements Mapper<int>
+ */
+class DoubleIntMapper implements Mapper
+{
+    public function __construct(private readonly MapperProvider $provider)
+    {
+    }
+
+    /**
+     * @param  list<string|int> $path
+     * @throws MappingFailedException
+     */
+    public function map(mixed $data, array $path = []): int
+    {
+        if (!is_int($data)) {
+            throw MappingFailedException::incorrectType($data, $path, 'int');
+        }
+
+        return MapToDouble::mapValue($data, $path);
+    }
+}
