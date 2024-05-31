2caecf4e8b387d076f220a0587bef4b745898b61
Michael Olšavský <molsavsky1@gmail.com>
2024-05-31 13:11:21 +0200

    Update src/Compiler/Validator/Array/AssertUniqueItems.php
    Co-authored-by: Jan Tvrdík <git@jantvrdik.com>

--- src/Compiler/Validator/Array/AssertUniqueItems.php
+++ src/Compiler/Validator/Array/AssertUniqueItems.php
@@ -24,7 +24,6 @@ class AssertUniqueItems implements ValidatorCompiler
             'index',
             'item',
             'innerIndex',
-            'innerLoopItem',
         );
 
         $statements = [];
